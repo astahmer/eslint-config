3990b6de32dba8c0451714a46ac25f503826044f
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2022-10-24 10:52:55 +0200

    refactor: react package ts-> js + use strictest tsconfig

--- packages/react/index.js
+++ packages/react/index.js
@@ -1,6 +1,7 @@
-import { defineConfig } from "eslint-define-config";
+// @ts-check
+const { defineConfig } = require("eslint-define-config");
 
-export default defineConfig({
+module.exports = defineConfig({
     root: true,
     parserOptions: {
         jsx: true,
